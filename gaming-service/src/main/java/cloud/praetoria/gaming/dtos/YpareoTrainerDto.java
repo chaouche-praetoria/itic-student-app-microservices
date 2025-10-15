2d5d7008ffc7b2262d9a1a989df79c162e6de974
Chéridanh <divintsiela@gmail.com>
2025-10-15 03:02:38 +0200

    gaming(trainers) : add dto for trainer

---
+++ gaming-service/src/main/java/cloud/praetoria/gaming/dtos/YpareoTrainerDto.java
@@ -0,0 +1,11 @@
+package cloud.praetoria.gaming.dtos;
+
+import lombok.Data;
+
+@Data
+public class YpareoTrainerDto {
+    private Long ypareoCode;
+    private String firstName;
+    private String lastName;
+    private Long codeGroupe;
+}
