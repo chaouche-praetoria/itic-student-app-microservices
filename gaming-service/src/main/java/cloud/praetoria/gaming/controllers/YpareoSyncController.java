b011f9904cef0dd3aba0610125270b7bcb504275
chaouche-praetoria <m.chaouche@praetoria.cloud>
2025-10-17 21:13:18 +0200

    Change Get to Post

--- gaming-service/src/main/java/cloud/praetoria/gaming/controllers/YpareoSyncController.java
+++ gaming-service/src/main/java/cloud/praetoria/gaming/controllers/YpareoSyncController.java
@@ -1,7 +1,12 @@
 package cloud.praetoria.gaming.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import cloud.praetoria.gaming.services.YpareoSyncService;
 import lombok.RequiredArgsConstructor;
@@ -13,19 +18,19 @@ public class YpareoSyncController {
 
     private final YpareoSyncService ypareoSyncService;
 
-    @GetMapping("/groups")
+    @PostMapping("/groups")
     public ResponseEntity<String> syncGroups() {
         ypareoSyncService.syncGroups();
         return ResponseEntity.ok("Groups synchronized");
     }
 
-    @GetMapping("/students")
+    @PostMapping("/students")
     public ResponseEntity<String> syncStudents() {
         ypareoSyncService.syncStudents();
         return ResponseEntity.ok("Students synchronized");
     }
 
-    @GetMapping("/trainers")
+    @PostMapping("/trainers")
     public ResponseEntity<String> syncTrainers() {
         ypareoSyncService.syncTrainers();
         return ResponseEntity.ok("Trainers synchronized");
